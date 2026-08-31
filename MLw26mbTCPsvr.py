ec7f2b8a7b7d78eed6617dad7a6b03699029bb76
nsrpta23ens <142359776+nsrpta23ens@users.noreply.github.com>
2026-08-31 16:28:08 +0700

    Add files via upload

--- MLw26mbTCPsvr.py
+++ MLw26mbTCPsvr.py
@@ -5,8 +5,8 @@ def updRegs():#print("BackgroundThread started.modifying inRegs..")
     while True:#generate some randomized SensorReadings temp=random.randint(0,999);#humi=random.randint(45,65)
         m3h=0 #dt=datetime.now()    # DataBank.set_List_words(addr,[List_of_16bit_ints]) #sets inRegs starting@aGivenAddress offset
         #DataBank.set_list_words(0,[dt.second]) #,humi]) #print(f"[SvrStatus]updRegs:Temp={temp}°C,Humid={humi}%")
-        with open("/home/pi/HN_MELI_NUOML1/HN_MELI_NUOML1_PMsCur.txt","r") as f: #HN_MELI_NUOML1_PMsCur.txt
-            Line=f.readline();m3h=int(Line.split("\t")[1])
+        with open("/home/pi/HN_MELI_NUOML1/HN_MELI_NUOML1_PMsCur.txt","rb",buffering=0) as f: #"r") as f:#Line=f.readline();
+            bs=f.read(32);Line=bs.decode("utf-8");m3h=int(Line.split("\t")[1])
             MBtcpSvr.data_bank.set_input_registers(0,[m3h]);time.sleep(3)
 if __name__ == "__main__":
     try:
