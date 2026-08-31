a625aa8d6f9aeb2e8f4231745cf548231f250a0a
nsrpta23ens <142359776+nsrpta23ens@users.noreply.github.com>
2026-08-31 10:17:00 +0700

    Add files via upload

---
+++ MLw26flow.py
@@ -0,0 +1,3 @@
+#2026au31mo@e77-MLw26flow.py tnhan@enersev
+with open("/home/pi/HN_MELI_NUOML1/HN_MELI_NUOML1_PMsCur.txt","r") as f: #HN_MELI_NUOML1_PMsCur.txt
+    Line=f.readline();m3h=int(Line.split("\t")[1]);print(m3h)
