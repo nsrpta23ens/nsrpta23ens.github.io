6ace2d42c1aa6e7a091de64e295c52d3bddec132
nsrpta23ens <142359776+nsrpta23ens@users.noreply.github.com>
2026-09-03 08:27:24 +0700

    Add files via upload

--- MLw26moni.py
+++ MLw26moni.py
@@ -15,7 +15,7 @@ def pmLogs(name,v,prec,unit,dt,sta):
 sPMname=["Flow in 1","TSS      ", "COD      ", "NH4+     ","DO       ","pH       ","Temp     "];   
 sPMunit=["m3/h"     ,"mg/L"     ,"mg/L"      ,"mg/L"      , "mg/L"    ,"pH"       ,"°C"];     
 fPMvalu=[0,0,0,0,0,0,0];fPMsum=[0,0,0,0,0,0,0];nPMcnt=[0,0,0,0,0,0,0];nPMtota=7
-nPMprec=[0,2,2,2,2,2,1];fPMavg=[0,0,0,0,0,0,0];sCWD="";sFNprefix="HN_MELI_NUOML1";sFn2sv="";dt2sv=datetime.now()
+nPMprec=[0,2,2,2,2,2,1];fPMavg=[0,0,0,0,0,0,0];sCWD="";sFNprefix="HN_MELI_NUOML1";sFn2sv="";sFn2fw="";dt2sv=datetime.now()
 def pmsHead():
     sh="DateTime"
     for i in range(nPMtota):sh+="\t"+sPMname[i]
@@ -27,7 +27,7 @@ def pmsVals(dt,v):
 def upLf2sv(fn,dt):#192.168.1.149/X.%20PLANT%20DATA/7.MeLinhWater/MLwQlity  2026au27thu@e77
     c="curl -s -k --ftp-create-dirs -T "+fn;c+=" ftp://14.177.182.250/nuocmat/"+dt.strftime("%Y/")+dt.strftime("%m/")+dt.strftime("%d/")+' -u "250nmmelinh08980:Melinh@072026"'
     ec=os.system(c);print(c+"\t ec"+str(ec))
-r1=bytes([1,3,0,0,0,2, 0xc4,0x0b]);rq=bytes([2,4,0,0,0,14,0x71,0xfd]) #([1,4,0,0,0,14,0x71,0xce]) #([1,4,0,0,0,10,0x70,0x0d])
+r1=bytes([1,3,0,0,0,2,0xc4,0x0b]);rq=bytes([2,4,0,0,0,14,0x71,0xfd]) #([1,4,0,0,0,14,0x71,0xce]) #([1,4,0,0,0,10,0x70,0x0d])
 while True:
     try:
         c="stty -F /dev/ttyS0 9600 cs8 -parenb -cstopb raw";ec=os.system(c)#;print(c+"\t ec"+str(ec))
@@ -46,13 +46,13 @@ while True:
         if os.path.exists(fn)==False:
             with open(fn,"a") as f:f.write(pmsHead()+"\n")
         with open(fn,"a") as f:    f.write(pmsVals(dt,fPMvalu)+"\n")#
-        fn=fc=ff=sCWD+sFNprefix+"_";fn+=dt.strftime("%Y%m%d%H")+".txt";fc+="PMsCur.txt";ff+=dt.strftime("%Y%m%d")+"flowin.txt" #"0000.txt"
+        fn=fc=fw=sCWD+sFNprefix+"_";fn+=dt.strftime("%Y%m%d%H")+".txt";fc+="PMsCur.txt";fw+=dt.strftime("%Y%m%d")+"flowin.txt" #"0000.txt"
         if sFn2sv!="" and os.path.exists(fn)==False:
             upLf2sv(sFn2sv,dt2sv) #print(sFn2sv+" up2sv"+dt2sv.strftime("%Y%m%d%H%M%S"));
             with open(sFn2sv,"r") as f:ss=f.readline()
-            with open(ff,"a") as f:f.write(ss)   #+"\n" 2026se2we@LvL 
+            with open(sFn2fw,"a") as f:f.write(ss)   #+"\n" 2026se2we@LvL 
             for i in range(np):nPMcnt[i]=0;fPMsum[i]=0 #2026se2we@LvL
-        sFn2sv=fn;dt2sv=dt;ss="";sc=""
+        sFn2sv=fn;sFn2fw=fw;dt2sv=dt;ss="";sc=""
         for i in range(np):
             nPMcnt[i]+=1;fPMsum[i]+=fPMvalu[i];fPMavg[i]=fPMsum[i]/nPMcnt[i]
             ss+=pmLogs(sPMname[i],fPMavg[i] ,nPMprec[i],sPMunit[i],dt,"01")+"\n"
